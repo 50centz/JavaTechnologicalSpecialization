4ce89ef228fe2df52765ee5f0d604d7c7df887de
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-11-19 02:14:27 +0300

    Sixth homework complete

---
+++ Java Development Kit/HomeWork6/Main.java
@@ -0,0 +1,12 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        MontyHallParadox montyHallParadox = new MontyHallParadox();
+        System.out.println();
+        MontyHallParadoxChangeSelection montyHallParadoxChangeSelection = new MontyHallParadoxChangeSelection();
+
+
+    }
+}
